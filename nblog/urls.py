7f2fbda4fc08350535629e642b16ae9277363804
Nataila <nataila.cc.814@gmail.com>
2017-09-28 11:49:27 +0800

    美化admin

--- nblog/urls.py
+++ nblog/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^ueditor/', include('DjangoUeditor.urls')),
+    url(r'^grappelli/', include('grappelli.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
