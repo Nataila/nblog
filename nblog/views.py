06ab13bd2b7f665d9227d6d2d4b40f1c8d9abb0e
Nataila <nataila.cc.814@gmail.com>
2017-10-12 16:43:04 +0800

    分页

--- nblog/views.py
+++ nblog/views.py
@@ -14,6 +14,7 @@ class HomeView(ListView):
     """
     template_name = 'nblog/home.html'
     model = Posts
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
